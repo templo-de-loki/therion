9baae988b67bc1d0f66a039933cdcdd2138ecd1c
Ed Castro <ed.sdr@outlook.com>
2023-09-09 22:38:17 -0300

    fix: background

--- components/Page/Gradient/index.js
+++ components/Page/Gradient/index.js
@@ -19,11 +19,16 @@ const Background = () => {
     width: "100%",
     height: "100%",
     position: "absolute",
-    "--gradient-color-1": "#022233",
-    "--gradient-color-2": "#021822",
-    "--gradient-color-3": "#196F4B",
-    "--gradient-color-4": "#093D42",
+    "--gradient-color-1": "#2e253f",
+    "--gradient-color-2": "#231e2e",
+    "--gradient-color-3": "#0c0b12",
+    "--gradient-color-4": "#211433",
   };
+
+  // "--gradient-color-1": "#022233",
+  // "--gradient-color-2": "#021822",
+  // "--gradient-color-3": "#196F4B",
+  // "--gradient-color-4": "#093D42",
 
   return (
     <canvas
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,20 +2,19 @@
 import { NextPage } from "next";
 import { Stack, Typography } from "@mui/material";
 import { PageWrapper } from "@/components/Page/Wrapper";
+import MediaCard from "@/components/MediaCard";
 
 const DashboardPage: NextPage = () => {
   return (
     <PageWrapper>
       <Stack
-        direction="column"
+        direction="row"
         justifyContent="center"
         alignItems="center"
         spacing={2}
       >
-        <img
-          src="https://i.pinimg.com/originals/78/a7/f5/78a7f5e0bd41db21ec8cf85fcc6102cd.gif"
-          alt="Loki"
-        />
+        <MediaCard />
+        <MediaCard />
       </Stack>
     </PageWrapper>
   );
