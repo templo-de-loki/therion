1302e6870e962c9af23f928c1b78fbc176879839
Ed Castro <ed.sdr@outlook.com>
2023-09-10 16:12:08 -0300

    feat: page container component

--- components/Page/Wrapper/index.tsx
+++ components/Page/Wrapper/index.tsx
@@ -1,29 +1,10 @@
 import Image from "next/image";
-import { Container, Stack } from "@mui/material";
+import { Stack } from "@mui/material";
 
-export interface PageWrapperProps {}
-
-export const PageWrapper = ({
-  children,
-}: React.PropsWithChildren<PageWrapperProps>) => {
+export const PageWrapper = ({ children }: React.PropsWithChildren) => {
   return (
     <>
-      <Stack flexGrow={1}>
-        <Stack
-          boxShadow={1}
-          borderRadius={1}
-          overflow="hidden"
-          justifyContent="center"
-          sx={(theme) => ({
-            padding: theme.spacing(4),
-            marginTop: theme.spacing(6),
-            minHeight: theme.spacing(10),
-            backgroundColor: theme.palette.background.default,
-          })}
-        >
-          <Container maxWidth="lg">{children}</Container>
-        </Stack>
-      </Stack>
+      <Stack flexGrow={1}>{children}</Stack>
 
       <Image
         src="/background.svg"
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,22 @@ import { NextPage } from "next";
 import { Stack } from "@mui/material";
 import { PageWrapper } from "@/components/Page/Wrapper";
 import MediaCard from "@/components/MediaCard";
+import { PageContainer } from "@/components/Page/Container";
 
 const MainPage: NextPage = () => {
   return (
     <PageWrapper>
-      <Stack
-        direction="row"
-        justifyContent="center"
-        alignItems="center"
-        spacing={2}
-      >
-        <MediaCard />
-        <MediaCard />
-      </Stack>
+      <PageContainer>
+        <Stack
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+          spacing={2}
+        >
+          <MediaCard />
+          <MediaCard />
+        </Stack>
+      </PageContainer>
     </PageWrapper>
   );
 };
