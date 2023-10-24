c872e53e56f939a8a9073078ce30521c0c13ba4b
Ed Castro <ed.sdr@outlook.com>
2023-10-23 23:51:02 -0300

    refactor: migrate to t3 app and bun

--- components/Loader/index.tsx
+++
@@ -1,9 +0,0 @@
-import { CircularProgress, Stack } from "@mui/material";
-
-const Loader = () => (
-  <Stack display="flex" alignItems="center" justifyContent="center">
-    <CircularProgress />
-  </Stack>
-);
-
-export default Loader;
